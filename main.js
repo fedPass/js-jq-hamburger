c5cf9337f53b0d4db4ded9b283b0fd50daf79189
fedPass <federica.passanante@gmail.com>
2019-11-25 17:48:12 +0100

    eliminata classe open e sostituito con selettore

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // sotto 1000px cliccando su icona bars si apre l'hamburger menu
-$('.open').click(function(){
+$('.header-right>a').click(function(){
     $('.hamburger-menu').addClass('active');
 });
 //nel hamburger-menu quando clicco su X si chiude la finestra e mi riappare l'icona bars
